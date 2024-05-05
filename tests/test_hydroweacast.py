8599e4467c918a318a03d7653501de01988bfc6b
ouyangwenyu <hust2014owen@gmail.com>
2024-05-05 17:31:45 +0800

    update env file

--- tests/test_hydroweacast.py
+++ tests/test_hydroweacast.py
@@ -1,16 +1,16 @@
 """
 Author: Wenyu Ouyang
 Date: 2024-04-09 16:31:35
-LastEditTime: 2024-04-09 16:40:40
+LastEditTime: 2024-05-05 16:39:12
 LastEditors: Wenyu Ouyang
 Description: Tests for `hydroweacast` package.
-FilePath: \hydroweacast\tests\test_hydroweacast.py
+FilePath: /hydroweacast/tests/test_hydroweacast.py
 Copyright (c) 2023-2024 Wenyu Ouyang. All rights reserved.
 """
 
 import pytest
 
-from hydroweacast.hydroweacast.run_graphcast import main
+from hydroweacast.run_graphcast import main
 
 
 def test_main():
