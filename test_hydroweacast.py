9243018b3a44a033aeb002020fa79f94eb90c87c
xw5555 <2277618640@qq.com>
2024-05-06 11:20:08 +0800

    0506

--- test_hydroweacast.py
+++ test_hydroweacast.py
@@ -9,10 +9,22 @@ Copyright (c) 2023-2024 Wenyu Ouyang. All rights reserved.
 """
 
 import pytest
+import time
 from hydroweacast.run_graphcast import GraphCast
+from jax import profiler
 
 def test_gc():
     gc = GraphCast()
     gc.param_path = 'params_GraphCast - ERA5 1979-2017 - resolution 0.25 - pressure levels 37 - mesh 2to6 - precipitation input and output.npz'
     gc.dataset_path = 'datasource_2024_steps_01.nc'
     gc.run()
+
+def test_profiler():
+    profiler.start_trace("/hydro_log/tensorboard")
+    for i in range(5):
+        time.sleep(1)
+    profiler.stop_trace()
+
+# profiler.start_trace("/tensorboard")
+test_gc()
+# profiler.stop_trace()
